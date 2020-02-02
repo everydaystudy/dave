9f67af761353b761512fc2fbc95c06ab5ced71c8
everydaystudy <37063919+everydaystudy@users.noreply.github.com>
2020-02-03 07:33:02 +0900

    Update test_dave.py

--- test_dave.py
+++ test_dave.py
@@ -5,7 +5,7 @@ import dave
 class ColumnNamesTest(unittest.TestCase):
  
     def test_add(self):
-        c = dave.return_column_names("iris.csv")
+        c = dave.column_names("iris.csv")
         self.assertEqual(c, ['sepal.length', 'sepal.width', 'petal.length', 'petal.width', 'variety'])
  
  
